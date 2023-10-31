42554f55181370a3717b1645205a49c6ec0bae80
Allon Nam <allon.nam96@gmail.com>
2023-10-31 14:16:11 -0400

    on and off

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,25 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+
+  const {themeName, setThemeName } = useTheme();
+
+  const handleClick = e => {
+    e.preventDefault();
+    console.log(e.target);
+    e.target.className === "on" ? setThemeName("day") : setThemeName("night");
+  }
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div>
+        <button onClick = {handleClick} className="on"> DAY </button>
+      </div>
+      <div>
+        <button onClick = {handleClick} className="off"> NIGHT </button>
+      </div>
+        
     </div>
   );
 }
