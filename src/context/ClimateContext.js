b13ee2de9a89f7c8dd57a90b1f4d4e87ddd5f7ba
Allon Nam <allon.nam96@gmail.com>
2023-10-31 14:35:42 -0400

    temperature

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,26 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+
+export const useTheme = () => useContext(ClimateContext);
+
+export default function ClimateProvider({ children }) {
+  const [climateTemperature, setClimateTemperature] = useState(50);
+  const [climateHumidity, setClimateHumidity] = useState(40);
+
+  return (
+    <ClimateContext.Provider
+      value={{
+        climateTemperature,
+        setClimateTemperature,
+        climateHumidity,
+        setClimateHumidity
+      }}
+    >
+      {children}
+    </ClimateContext.Provider>
+  );
+}
